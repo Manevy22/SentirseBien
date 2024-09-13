64417ceab57b2147883c1a6fba5392a72aad7fa3
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2024-09-13 19:57:49 -0300

    Update app.js

--- SentirseBien/app.js
+++ SentirseBien/app.js
@@ -7,11 +7,11 @@ app.use(express.static(path.join(__dirname)));
 
 // Servir index.html cuando se acceda a la raíz
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Iniciar servidor en el puerto 8080
-const PORT = 8080;
+// Usar el puerto proporcionado por Railway o el puerto 8080 para desarrollo local
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://127.0.0.1:${PORT}`);
 });
