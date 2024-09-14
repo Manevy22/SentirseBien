b372f6a467c65727ae0baaa9f9c20911acb2ba0f
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2024-09-14 00:54:36 -0300

    Update ClienteController.java

--- nuevo/src/main/java/com/madeTUP/AppSpa/Controller/ClienteController.java
+++ nuevo/src/main/java/com/madeTUP/AppSpa/Controller/ClienteController.java
@@ -53,7 +53,7 @@ public class ClienteController {
         return servis.getClientes();
     }
             
-    @CrossOrigin(origins = "https://overflowing-magic-production.up.railway.app")
+    @CrossOrigin(origins = "*")
     @PostMapping("/clientes/crear")
     public String crearCliente(@RequestBody Cliente c){
         servis.saveCliente(c);
@@ -116,7 +116,7 @@ public class ClienteController {
        servis4.editConsultaII(co);
          return "Consulta Agregada";
      }
-     @CrossOrigin(origins = "https://overflowing-magic-production.up.railway.app")
+     @CrossOrigin(origins = "*")
     @PostMapping("/Cliente/login")
 public ResponseEntity<Map<String, Object>> loginCliente(@RequestBody ClienteLoginDTO clienteDTO) {
     List<Cliente> listaClientes = servis.getClientes();
@@ -138,7 +138,7 @@ public ResponseEntity<Map<String, Object>> loginCliente(@RequestBody ClienteLogi
 }
 
 
-@CrossOrigin(origins = "https://overflowing-magic-production.up.railway.app")
+@CrossOrigin(origins = "*")
 @GetMapping("/cliente/perfil")
     public ResponseEntity<ClientePerfilDTO> getPerfilCliente(@RequestParam Long clienteId) {
         Cliente cliente1= servis.findCliente(clienteId);
