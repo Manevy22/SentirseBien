bae1f73d89ad7bb9f186e7a903807a25f11f4228
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2024-09-14 00:48:04 -0300

    Update registrar.js

--- SentirseBien/js/registrar.js
+++ SentirseBien/js/registrar.js
@@ -28,7 +28,7 @@ document.querySelector('form').addEventListener('submit', function (event) {
     };
 
     // Realiza la petición POST a la API
-    fetch('https://sentirsebien-production.up.railway.app:8081/clientes/crear', requestOptions)
+    fetch('https://overflowing-magic-production.up.railway.app/clientes/crear', requestOptions)
     .then(response => {
         if (response.status === 201) {
             return response.json().then(data => alert('Usuario creado exitosamente.'));
