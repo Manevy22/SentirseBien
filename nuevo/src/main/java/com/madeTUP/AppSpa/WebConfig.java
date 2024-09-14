7db6396513c9e10b2600f9ec5fa3d6297b3afcfe
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2024-09-13 21:28:11 -0300

    Update WebConfig.java

--- nuevo/src/main/java/com/madeTUP/AppSpa/WebConfig.java
+++ nuevo/src/main/java/com/madeTUP/AppSpa/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080")
+                .allowedOrigins("https://just-luck-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 
