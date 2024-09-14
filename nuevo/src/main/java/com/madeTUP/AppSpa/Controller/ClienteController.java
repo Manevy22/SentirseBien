87fc2d65645a399a2b4bf57906b8d8a6a915b094
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2024-09-13 23:17:41 -0300

    Update ClienteController.java

--- nuevo/src/main/java/com/madeTUP/AppSpa/Controller/ClienteController.java
+++ nuevo/src/main/java/com/madeTUP/AppSpa/Controller/ClienteController.java
@@ -53,7 +53,7 @@ public class ClienteController {
         return servis.getClientes();
     }
             
-    @CrossOrigin(origins = "https://just-luck-production.up.railway.app:8081")
+    @CrossOrigin(origins = "https://sentirsebien-production.up.railway.app:8081")
     @PostMapping("/clientes/crear")
     public String crearCliente(@RequestBody Cliente c){
         servis.saveCliente(c);
@@ -116,7 +116,7 @@ public class ClienteController {
        servis4.editConsultaII(co);
          return "Consulta Agregada";
      }
-     @CrossOrigin(origins = "https://just-luck-production.up.railway.app:8081")
+     @CrossOrigin(origins = "https://sentirsebien-production.up.railway.app:8081")
     @PostMapping("/Cliente/login")
 public ResponseEntity<Map<String, Object>> loginCliente(@RequestBody ClienteLoginDTO clienteDTO) {
     List<Cliente> listaClientes = servis.getClientes();
