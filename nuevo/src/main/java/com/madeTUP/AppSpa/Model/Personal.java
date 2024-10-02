9dabbda086f583c02611a0612733414f8fa5c92c
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2024-10-02 00:44:47 -0300

    Update Personal.java

--- nuevo/src/main/java/com/madeTUP/AppSpa/Model/Personal.java
+++ nuevo/src/main/java/com/madeTUP/AppSpa/Model/Personal.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.madeTUP.AppSpa.Model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
@@ -11,9 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import java.util.List;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,35 +18,32 @@ import lombok.Setter;
 @Entity
 @Getter @Setter
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-
 public class Personal {
+
     @Id
-@GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    
     private String nombre;
     private String apellido;
     private String correo;
     private String nombre_usuario;
     private String contrasenia;
-    @ManyToMany
-    @JoinTable(name = "empleado_servicio", joinColumns = @JoinColumn(name = "empleado_id"),inverseJoinColumns = @JoinColumn(name = "servicio_id"))
-            private List<Servicio> listaServicio;
     
+    @OneToOne
+    @JoinColumn(name = "servicio_id")  // Define la columna que será clave aaforánea
+    private Servicio servicio;  // Cambiado a una relación OneToOne con Servicio
 
     public Personal() {
     }
 
-    public Personal(Long id, String nombre, String apellido, String correo, String nombre_usuario, String contrasenia, List<Servicio> listaServicio) {
+    public Personal(Long id, String nombre, String apellido, String correo, String nombre_usuario, String contrasenia, Servicio servicio) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
         this.nombre_usuario = nombre_usuario;
         this.contrasenia = contrasenia;
-        this.listaServicio = listaServicio;
+        this.servicio = servicio;
     }
-
-    
-    
-
 }
