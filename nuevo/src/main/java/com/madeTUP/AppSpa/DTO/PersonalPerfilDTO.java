9ca22428392d4dba86392bf19a7b5fda1c0b298d
Manevy22 <141934385+Manevy22@users.noreply.github.com>
2024-10-05 21:31:53 -0300

    Update PersonalPerfilDTO.java

--- nuevo/src/main/java/com/madeTUP/AppSpa/DTO/PersonalPerfilDTO.java
+++ nuevo/src/main/java/com/madeTUP/AppSpa/DTO/PersonalPerfilDTO.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.madeTUP.AppSpa.DTO;
 
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-import com.madeTUP.AppSpa.DTO.SesionPersonalDTO;
 
 /**
  *
@@ -16,8 +11,7 @@ import com.madeTUP.AppSpa.DTO.SesionPersonalDTO;
 @Getter
 @Setter
 public class PersonalPerfilDTO {
-   
-     private Long id;
+    private Long id;
     private String nombre_usuario;
     private List<SesionPersonalDTO> listaPer_Ses;
 
@@ -29,26 +23,4 @@ public class PersonalPerfilDTO {
         this.nombre_usuario = nombre_usuario;
         this.listaPer_Ses = listaPer_Ses;
     }
-   @GetMapping("/personal/perfil")
-public ResponseEntity<PersonalPerfilDTO> getPerfilPersonal(@RequestParam Long PersonalId) {
-    Personal personal1 = servis.findPersonal(PersonalId);
-    
-    if (personal1 == null) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-    }
-
-   
-    PersonalPerfilDTO perfilDTO = new PersonalPerfilDTO();
-    perfilDTO.setId(PersonalId);
-    perfilDTO.setNombre_usuario(personal1.getNombre_usuario()); 
-
-    List<SesionPersonalDTO> listaSesionesDos = new ArrayList<>();
-   
-
-    perfilDTO.setListaPer_Ses(listaSesionesDos);
-
-    return ResponseEntity.ok(perfilDTO);
-}
-
-  
 }
